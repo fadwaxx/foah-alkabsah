3eb0b40153db4368b9698ef3fade11c8a0b98e5c
fadwaxx <158286618+fadwaxx@users.noreply.github.com>
2026-06-12 15:50:32 +0300

    Refactor static file serving and image path in app.js
    Updated static file serving and modified image path handling.

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "frontend")));
 
   app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
@@ -31,7 +32,7 @@ app.use(express.json());
   app.post("/api/menu", upload.single("image"), async (req, res) => {
     try {
       const imagePath = req.file
-        ? `http://localhost:5000/uploads/${req.file.filename}`
+        ? `/uploads/${req.file.filename}`
         : "";
   
       let options = [];
@@ -88,7 +89,7 @@ app.use(express.json());
       };
   
       if (req.file) {
-        updateData.image = `http://localhost:5000/uploads/${req.file.filename}`;
+        updateData.image = `/uploads/${req.file.filename}`;
       }
   
       const item = await MenuItem.findByIdAndUpdate(
@@ -375,9 +376,12 @@ mongoose.connect(process.env.MONGO_URI)
 });
 
 app.get("/", (req, res) => {
-    res.send("فوح الكبسة Backend يعمل بنجاح 🚀");
-});
+    res.sendFile(
+      path.join(__dirname, "frontend", "foah-alkabsah (1).html")
+    );
+  });
+const PORT = process.env.PORT || 5000;
 
-app.listen(process.env.PORT, () => {
-    console.log(`🚀 Server running on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
